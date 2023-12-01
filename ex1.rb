ce7917468faa2c390f8cbf85e33e02912d4cd5f6
Hillel Kahana <hillelk@wix.com>
2023-12-01 13:33:14 +0200

    ex1_beautify

--- ex1.rb
+++ ex1.rb
@@ -1,6 +1,7 @@
 
 FILE='input1'
-def r(s)
+
+def replace(s)
   s.gsub('one','1')
    .gsub('two','2')
    .gsub('three', '3')
@@ -13,21 +14,18 @@ def r(s)
 end
 
 def format_str(s, s2i)
-  return s.gsub(/[a-z]/,'') if !s2i
+  return s.gsub(/[a-z]/,'').chars if !s2i
 
-  return r(s.scan(/(?=(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9))/).flatten.join).gsub(/[a-z]/,'')
+  s.scan(/(?=(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9))/).flatten.map(&method(:replace))
 end
 
 def part(fmt=false)
-	sum=0
-	File.read(FILE).each_line {|l|
-	  arr = []
-
-	  format_str(l.chop, fmt).each_char {|c| arr << c.to_i}
-
-	  sum += arr.last + arr.first*10
-	}
-	sum
+  sum=0
+  File.read(FILE).each_line {|l|
+    arr = format_str(l.chop, fmt)
+    sum += arr.last.to_i + arr.first.to_i*10
+  }
+  sum
 end
 
 
