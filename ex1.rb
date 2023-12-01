65af151da7f59bf513cc9e924d22aea400fffd25
Hillel Kahana <hillelk@wix.com>
2023-12-01 13:59:31 +0200

    compact

--- ex1.rb
+++ ex1.rb
@@ -1,22 +1,13 @@
 
 FILE='input1'
 
-def replace(s)
-  s.gsub('one','1')
-   .gsub('two','2')
-   .gsub('three', '3')
-   .gsub('four', '4')
-   .gsub('five', '5')
-   .gsub('six', '6')
-   .gsub('seven', '7')
-   .gsub('eight', '8')
-   .gsub('nine', '9')
-end
+NUMS=['','one','two','three','four','five','six','seven', 'eight', 'nine']
+def replace(s); NUMS.index(s) || s; end
 
 def format_str(s, s2i)
   return s.gsub(/[a-z]/,'').chars if !s2i
 
-  s.scan(/(?=(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9))/).flatten.map(&method(:replace))
+  s.scan(/(?=(#{NUMS[1..-1].join("|")}|1|2|3|4|5|6|7|8|9))/).flatten.map(&method(:replace))
 end
 
 def part(fmt=false)
